f5b670c0bfe069548e593fd991ad7c82fdd777e5
Shishir Horane <shishirh@microsoft.com>
2018-11-01 16:27:48 -0700

    Add factorial

--- PortableTestAppSLCM1/PortableTestAppSLCM1/Math.cs
+++ PortableTestAppSLCM1/PortableTestAppSLCM1/Math.cs
@@ -4,9 +4,20 @@ namespace PortableTestAppSLCM1
 {
     public class Math
     {
+        public static long Factorial(int x)
+        {
+            if (x <= 1)
+            {
+                return 1;
+            }
+
+            return x * Factorial(x - 1);
+        }
+
         public static int Sum(int a, int b)
         {
             return a + b;
         }
+
     }
 }
