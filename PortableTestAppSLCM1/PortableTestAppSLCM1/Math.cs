a8b77cd6c0eac0c152a1e277676e46b2543a4b44
Cristina Manu <cmanu@microsoft.com>
2018-10-31 20:53:46 -0700

    Test

---
+++ PortableTestAppSLCM1/PortableTestAppSLCM1/Math.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PortableTestAppSLCM1
+{
+    public class Math
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
